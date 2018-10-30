1c57cd87d20263d6161572c6e858875d007a6287
henryco <henrycodev@gmail.com>
2018-10-30 15:37:17 +0100

    Work on jpa-like ORM

--- lib/util/model.dart
+++ lib/util/model.dart
@@ -2,48 +2,44 @@ import 'package:flutter/material.dart';
 
 export './util.dart';
 
-abstract class ViewState
-<WIDGET extends StatefulWidget, LOGIC> extends State<WIDGET> {
-	
-	ViewLogic <WIDGET, ViewState<WIDGET, LOGIC>> $logic;
-	LOGIC get $ => $logic as LOGIC;
-	
-	/// Overridable
-	ViewLogic <WIDGET, ViewState<WIDGET, LOGIC>> createLogic();
-	
-	@override
-	void initState() {
-		super.initState();
-		this.$logic = createLogic();
-		$logic._bind(this);
-	}
-	
-	@protected
-	void _update(VoidCallback callback) {
-		this.setState(callback);
-	}
-	
+abstract class ViewState<WIDGET extends StatefulWidget, LOGIC>
+    extends State<WIDGET> {
+  ViewLogic<WIDGET, ViewState<WIDGET, LOGIC>> $logic;
+
+  LOGIC get $ => $logic as LOGIC;
+
+  /// Overridable
+  @protected
+  ViewLogic<WIDGET, ViewState<WIDGET, LOGIC>> createLogic();
+
+  @override
+  void initState() {
+    super.initState();
+    this.$logic = createLogic();
+    $logic._bind(this);
+  }
+
+  @protected
+  void _update(VoidCallback callback) => this.setState(callback);
 }
 
+abstract class ViewLogic<WIDGET extends StatefulWidget, U extends ViewState> {
+  BuildContext get $context => $state.context;
 
-abstract class ViewLogic
-<WIDGET extends StatefulWidget, U extends ViewState> {
-	
-	BuildContext get $context => $state.context;
-	WIDGET get $ => $state.widget;
-	U $state;
-	
-	@protected
-	void _bind(U state) async {
-		this.$state = state;
-		this.initialize();
-	}
-	
-	/// Overridable
-	void initialize() { }
-	
-	void update(VoidCallback callback) {
-		$state._update(callback);
-	}
-	
+  WIDGET get $ => $state.widget;
+  U $state;
+
+  @protected
+  void _bind(U state) {
+    this.$state = state;
+    this.initialize();
+  }
+
+  /// Overridable
+  @protected
+  void initialize() {
+    /* void */
+  }
+
+  void update(VoidCallback callback) => $state._update(callback);
 }
